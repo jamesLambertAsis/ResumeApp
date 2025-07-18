c1349b7f97236a1e2ae66b0a3871db398b1ba168
James Lambert Asis <james.asis@tlcfzc.ae>
2025-07-18 16:19:26 +0800

    chore: camera access

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         }
 
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             isCrunchPngs = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -69,6 +69,9 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.firebase.firestore.ktx)
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -85,8 +88,10 @@ dependencies {
     implementation(platform(libs.firebase.bom))
 
     //koin
+    implementation(libs.koin.androidx.compose)
+    implementation(libs.koin.androidx.compose.navigation)
 
-        implementation(libs.koin.androidx.compose)
-        implementation(libs.koin.androidx.compose.navigation)
+    implementation (libs.barcode.scanning)
+    implementation (libs.androidx.camera.camera2)
 
 }
